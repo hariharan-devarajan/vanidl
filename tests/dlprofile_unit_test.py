132cad8a8fabd03b26aae76778b1d9d91c74ab4b
HariharanDevarajan <mani.hariharan@gmail.com>
2020-06-13 11:46:39 -0500

    added license and module details.

--- tests/dlprofile_unit_test.py
+++ tests/dlprofile_unit_test.py
@@ -1,7 +1,35 @@
+"""
+ Copyright (C) 2020  Argonne, Hariharan Devarajan <hdevarajan@anl.gov>
+ This file is part of DLProfile
+ HFetch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+System Includes
+"""
 import unittest
 import os
+import sys
+import pathlib
+
+sys.path.insert(0, pathlib.Path(__file__).parent.parent.absolute())
+
+"""
+Local Includes
+"""
 from src.dlprofile import DLProfile
 from src.constants import *
+
+
+
 
 
 def LoadEnv():
