a56d50cb6e6347c4a01bc8c0af81689490178899
Hariharan Devarajan <dhari@thetalogin4.tmi.alcf.anl.gov>
2020-06-14 22:56:11 +0000

    added install packages

--- setup.py
+++ setup.py
@@ -1,63 +1,21 @@
-#!/usr/bin/env python
-import sys
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+import setuptools
 
-# explicitly config
-test_args = [
-    '--cov-report=term',
-    '--cov-report=html',
-    '--cov=vanidl',
-    'tests'
-]
+long_description = ""
 
-
-class PyTest(TestCommand):
-    user_options = []
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = test_args
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-setup(name='vanidl',
-      version='0.0.1',
-      description='Deep Learning Analyzer for HPC Applications',
-      author='Hariharan Devarajan',
-      author_email='hdevarajan@hawk.iit.edu',
-      url='https://github.com/hariharan-devarajan/dlprofiler',
-      download_url='https://github.com/hariharan-devarajan/dlprofiler/tarball/0.0.1',
-      license='MIT',
-      packages=find_packages(exclude=['tests*']),
-      install_requires=[
-          'numpy',
-          'pandas',
-          'h5py',
-          'tensorflow'
-      ],
-      test_suite='tests',
-      cmdclass={'test': PyTest},
-      classifiers=[
-          'Programming Language :: Python',
-          'Operating System :: OS Independent',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence'
-      ],
-      keywords=[
-          'VaniDL',
-          'Darshan Analysis',
-          'Scientific',
-          'TensorFlow',
-          'Deep Learning',
-          'Machine Learning',
-          'Neural Networks',
-          'AI'
-      ]
-      )
+setuptools.setup(
+    name="vanidl", # Replace with your own username
+    version="0.0.1",
+    author="Hariharan Devarajan",
+    author_email="hdevarajan@hawk.iit.edu",
+    description="I/O analyzer of Scientific Deep Learning applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hariharan-devarajan/dlprofiler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
